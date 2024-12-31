afb8e677fd82c0c3c1c44760f95053767231e2f9
seokjun <seokjunh97@gmail.com>
2024-12-31 11:08:40 +0900

    Link 추가

--- namooinc.com/src/components/BusinessIntro.tsx
+++ namooinc.com/src/components/BusinessIntro.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const BusinessIntro = () => {
   return (
-    <div className="w-full px-4 py-8 lg:px-[28.125rem] md:py-16">
+    <div className="w-full px-4 py-8 md:py-16 lg:px-[28.125rem]">
       <div className="mb-[3.125rem] text-3xl font-black lg:text-6xl">
         Business
       </div>
       <div className="grid grid-cols-2 gap-4">
         <div className="relative aspect-square overflow-hidden">
-          <button className="group h-full w-full">
+          <Link href={"/corecode"} className="group h-full w-full">
             <div className="relative h-full w-full duration-500 group-hover:scale-110">
               <Image
                 src="/images/img2.png"
@@ -21,12 +22,12 @@ const BusinessIntro = () => {
             <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl text-white">
               CoreCode1
             </div>
-          </button>
+          </Link>
         </div>
 
         <div className="grid aspect-square grid-cols-2 gap-2">
           <div className="relative overflow-hidden">
-            <button className="group h-full w-full">
+            <Link href={"/corecode"} className="group h-full w-full">
               <div className="relative h-full w-full duration-500 group-hover:scale-110">
                 <Image
                   src="/images/img2.png"
@@ -36,13 +37,13 @@ const BusinessIntro = () => {
                   className="object-cover"
                 />
               </div>
-              <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xs text-white md:text-xl">
+              <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl text-white">
                 CoreCode2
               </div>
-            </button>
+            </Link>
           </div>
           <div className="relative overflow-hidden">
-            <button className="group h-full w-full">
+            <Link href={"/corecode"} className="group h-full w-full">
               <div className="relative h-full w-full duration-500 group-hover:scale-110">
                 <Image
                   src="/images/img2.png"
@@ -52,13 +53,13 @@ const BusinessIntro = () => {
                   className="object-cover"
                 />
               </div>
-              <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xs text-white md:text-xl">
+              <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl text-white">
                 CoreCode3
               </div>
-            </button>
+            </Link>
           </div>
           <div className="relative overflow-hidden">
-            <button className="group h-full w-full">
+            <Link href={"/corecode"} className="group h-full w-full">
               <div className="relative h-full w-full duration-500 group-hover:scale-110">
                 <Image
                   src="/images/img2.png"
@@ -68,13 +69,13 @@ const BusinessIntro = () => {
                   className="object-cover"
                 />
               </div>
-              <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xs text-white md:text-xl">
+              <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl text-white">
                 CoreCode4
               </div>
-            </button>
+            </Link>
           </div>
           <div className="relative overflow-hidden">
-            <button className="group h-full w-full">
+            <Link href={"/corecode"} className="group h-full w-full">
               <div className="relative h-full w-full duration-500 group-hover:scale-110">
                 <Image
                   src="/images/img2.png"
@@ -84,10 +85,10 @@ const BusinessIntro = () => {
                   className="object-cover"
                 />
               </div>
-              <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xs text-white md:text-xl">
+              <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl text-white">
                 CoreCode5
               </div>
-            </button>
+            </Link>
           </div>
         </div>
       </div>
